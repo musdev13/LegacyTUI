b650cafd5754945a93fa7a32904b2d31ccc8a1e1
MusDev13 <sonics0977703996@gmail.com>
2025-12-01 23:58:53 +0200

    minecraft launch

---
+++ src/ui/instances/instance.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+
+namespace TermLComp.Instances
+{
+    public partial class UI
+    {
+        public static void Instance(string instanceName)
+        {
+            string instancePath = Path.Combine(Path.Combine(TermLComp.Methods.WorkspaceDir(),"instances"),instanceName);
+            string? instanceVersion = TermLComp.Methods.GetFromFile(Path.Combine(instancePath,"TermL_data"),0);
+
+            Console.Clear();
+            Console.WriteLine($"{instanceName}\nversion: {instanceVersion}");
+            
+            string command = "java";
+            string args = $"-jar \"{Path.Combine(TermLComp.Methods.WorkspaceDir(), "bootstrap.jar")}\" --launch --directory \"{instancePath}\" --version \"{instanceVersion}\"";
+            Console.WriteLine($"{command} {args}");
+
+            var psi = new ProcessStartInfo()
+            {
+                FileName = command,
+                Arguments = args,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            using var process = new Process();
+            process.StartInfo = psi;
+
+            process.OutputDataReceived += (_, e) =>
+            {
+                if (e.Data != null)
+                    Console.WriteLine(e.Data);
+            };
+
+            process.ErrorDataReceived += (_, e) =>
+            {
+                if (e.Data != null)
+                    Console.WriteLine("Error: " + e.Data);
+            };
+
+            process.Start();
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+        }
+    }
+}
--- src/ui/instances/instances.cs
+++ src/ui/instances/instances.cs
@@ -13,7 +13,14 @@ namespace TermL
                 showOptions($"Instances {curPage}/{pages.Count-1}",pages[curPage].ToArray(), "Back", new string[] {"Next","Prev","Create Instance"});
                 char choice = getChar();
 
-                if (choice >= '1' && choice <= '4') continue;
+                if (choice >= '1' && choice <= '4')
+                {
+                    int index = choice - '1';
+                    if (index < pages[curPage].Count)
+                    {
+                        TermLComp.Instances.UI.Instance(pages[curPage][index]);
+                    }
+                }
 
                 switch (choice)
                 {
@@ -28,6 +35,7 @@ namespace TermL
                     case 'c':
                         TermLComp.Instances.UI.createInstance();
                         pages = TermLComp.Instances.Methods.init();
+                        TermLComp.Instances.UI.Instance(pages[0][0]);
                         break;
                 }
             }
